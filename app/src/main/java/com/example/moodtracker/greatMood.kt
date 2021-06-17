25c3876ce60942bb3d294b40079d85564c485553
zood234 <zainmahmood@live.co.uk>
2021-06-17 09:11:01 +0100

    Create all other activitys

---
+++ app/src/main/java/com/example/moodtracker/greatMood.kt
@@ -0,0 +1,11 @@
+package com.example.moodtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class greatMood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_great_mood)
+    }
+}
---
+++ app/src/main/java/com/example/moodtracker/veryBadMood.kt
@@ -0,0 +1,11 @@
+package com.example.moodtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class veryBadMood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_very_bad_mood)
+    }
+}
